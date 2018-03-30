5671e71c45763f43ecc883520d522b262d72ae91
Ian Pougher <ianpougher@gmail.com>
2018-03-29 20:51:30 -0600

    adds subLists and Countelements functions

--- Lists.hs
+++ Lists.hs
@@ -29,14 +29,48 @@ module Lists where
           zs = drop n xs
         in
           zs++ys
+--slice function slices the given positions out of the list
+  slice :: (Int,Int) -> [Int] -> [Int]
+  slice (a , b) xs = 
+      let
+          y=a-1
+      in
+      take (b - y + 1) (drop y xs)
+--subdivides the list into each iteration
+  subLists :: [Int]->[[Int]]
+  subLists xs =
+    let
+        size= length xs
+        start= 1
+    in
+        recurselist start size xs
+--recursive function that acts as the real subLists function
+  recurselist :: Int -> Int -> [Int]->[[Int]]
+  recurselist n z xs | n > z =[[]]
+                     | otherwise = take n xs : recurselist (n+1) z xs
+-- counts all of the sub lists elements and outputs one value
+  countElements:: [[Int]]->Int
+  countElements xs=
+      let 
+          size=length xs
+          value=1
+          iter=1
+      in
+          reCount size iter value xs
+          
+--recursive function that acts as the real countElements function
+  reCount:: Int -> Int -> Int ->[[Int]] -> Int
+  reCount size iterations value xs | iterations == size =  value
+                                   | otherwise = reCount size (iterations+1) ((length(xs!!iterations))+value) xs
+--sorts the system as a 
 --checks if the number given is a prime number, helper function made for the primeNumbers function
   isPrime:: Integer -> Bool
   isPrime n = 
       let 
           --sqrt wont allow an int, so you have to convert it beforehand
-          a = fromIntegral n
+        a = fromIntegral n
       in
-      ([]==[y | y<-[2..floor(sqrt a)], mod n y ==0])
+        ([]==[y | y<-[2..floor(sqrt a)], mod n y ==0])
 --couldn't find an elegant solution for doubling in the even numbers function, so I made this
 --helper function
   doublen :: Int -> Int
